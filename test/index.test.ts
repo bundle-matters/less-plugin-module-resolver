28b788a76ff02e4e4e4fe24da494cc166f4a9643
vagusX <vagusxl@gmail.com>
2022-05-15 14:14:00 +0800

    test: for less sync import

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,9 +5,17 @@ import fs from 'fs';
 
 import { LessPluginModuleResolver, LessPluginModuleResolverConfigs } from '../src';
 
-function lessc(filepath: string, pluginOptions: LessPluginModuleResolverConfigs) {
+async function lessc(filepath: string, pluginOptions: LessPluginModuleResolverConfigs) {
   const fileContent = fs.readFileSync(path.resolve(__dirname, 'fixtures/less', filepath), 'utf-8');
   return less.render(fileContent, {
+    plugins: [new LessPluginModuleResolver(pluginOptions)],
+  });
+}
+
+async function lesscSync(filepath: string, pluginOptions: LessPluginModuleResolverConfigs) {
+  const fileContent = fs.readFileSync(path.resolve(__dirname, 'fixtures/less', filepath), 'utf-8');
+  return less.render(fileContent, {
+    syncImport: true,
     plugins: [new LessPluginModuleResolver(pluginOptions)],
   });
 }
@@ -16,7 +24,7 @@ function getCssFilename(filepath: string) {
   return fs.readFileSync(path.resolve(__dirname, 'fixtures/css', filepath), 'utf-8');
 }
 
-describe('test cases', () => {
+describe('test cases for async', () => {
   describe('use normal string', () => {
     it('string replacement', async () => {
       const { css } = await lessc('basic.less', {
@@ -54,6 +62,58 @@ describe('test cases', () => {
 
     it('replacer function', async () => {
       const { css } = await lessc('basic.less', {
+        alias: {
+          '^styles/(.+)$': (_, restPath) => {
+            return path.resolve(__dirname, `fixtures/less/styles/${restPath}`);
+          },
+          '^@mixins$': () => {
+            return path.resolve(__dirname, 'fixtures/less/styles/mixins/index.less');
+          },
+        },
+      });
+      expect(css).toBe(getCssFilename('basic.css'));
+    });
+  });
+});
+
+describe('test cases for sync', () => {
+  describe('use normal string', () => {
+    it('string replacement', async () => {
+      const { css } = await lesscSync('basic.less', {
+        root: path.resolve(__dirname, 'fixtures'),
+        alias: {
+          styles: 'less/styles',
+          '@mixins': 'less/styles/mixins/index.less',
+        },
+      });
+      expect(css).toBe(getCssFilename('basic.css'));
+    });
+
+    it('replacer function', async () => {
+      const { css } = await lesscSync('basic.less', {
+        alias: {
+          styles: (_, restPath) => path.resolve(__dirname, `fixtures/less/styles${restPath}`),
+          '@mixins': () => path.resolve(__dirname, 'fixtures/less/styles/mixins/index.less'),
+        },
+      });
+      expect(css).toBe(getCssFilename('basic.css'));
+    });
+  });
+
+  describe('use regex string', () => {
+    it('string replacement', async () => {
+      const { css } = await lesscSync('basic.less', {
+        root: path.resolve(__dirname, 'fixtures'),
+        alias: {
+          '^styles(/.+)': 'less/styles',
+          '^@mixins$': () => 'less/styles/mixins/index.less',
+        },
+      });
+      expect(css).toBe(getCssFilename('basic.css'));
+    });
+
+    it('replacer function', async () => {
+      const { css } = await lesscSync('basic.less', {
         alias: {
           '^styles/(.+)$': (_, restPath) => {
             return path.resolve(__dirname, `fixtures/less/styles/${restPath}`);
