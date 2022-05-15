108754c52e717e4122979ec1f60ed359f83414aa
vagusX <vagusxl@gmail.com>
2022-05-15 19:39:26 +0800

    chore: update semantic release config

--- release.config.js
+++ release.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   branches: ['main'],
+  preset: 'angular',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        assets: [{ path: 'lib/**', label: 'JS distribution' }],
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+      },
+    ],
+  ],
 };
